f919d32a0cbe3fcf7638f1bcc5babc325b472462
frankuzi <wasabi555579@gmail.com>
2021-03-23 00:20:18 +0300

    Homework: new list class

---
+++ ConsoleApp1/ConsoleApp1/OmegaList.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class OmegaList
+    {
+        private static int[] newList = new int[0];
+
+        public static int Count => newList.Length;
+
+        public OmegaList(params int[] args)
+        {
+            newList = new int[args.Length];
+            foreach (var i in newList)
+            {
+                newList[i] = args[i];
+            }
+        }
+
+        public static int[] AddElement(int number)
+        {
+            var oneMoreList = new int[newList.Length + 1];
+            newList.CopyTo(oneMoreList, 0);
+            oneMoreList[oneMoreList.Length - 1] = number;
+            newList = oneMoreList;
+            return newList;
+        }
+
+        public static int[] DeleteElement(int num)
+        {
+            for (int i = num; i < newList.Length; i++)
+            {
+                if(i != newList.Length - 1)
+                newList[i] = newList[i + 1];
+            }
+            var oneMoreList = new int[newList.Length - 1];
+            newList.CopyTo(oneMoreList, 0);
+
+            newList = oneMoreList;
+            return newList;
+        }
+
+        public static bool Contains(int number)
+        {
+            foreach (int i in newList)
+            {
+                return newList[i] == number;
+            }
+
+            return false;
+        }
+
+        public static int[] Clear()
+        {
+            newList = null;
+            return newList;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static OmegaList newList = new OmegaList (1, 2 ,3 );
+        
+        static void Main(string[] args)
+        {
+            newList = new OmegaList(OmegaList.AddElement(5));
+            newList = new OmegaList(OmegaList.AddElement(4));
+            newList = new OmegaList(OmegaList.AddElement(1));
+            newList = new OmegaList(OmegaList.AddElement(23));
+            newList = new OmegaList(OmegaList.AddElement(34));
+            newList = new OmegaList(OmegaList.AddElement(55));
+            //newList = new OmegaList(OmegaList.DeleteElement(2));
+            Console.WriteLine(newList);
+            Console.ReadKey();
+        }
+        
+    }
+}
